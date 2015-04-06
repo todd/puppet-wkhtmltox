9965b850326af3b911518cf4c7aff1cb496da09a
Todd Bealmear <todd@t0dd.io>
2015-04-06 13:56:36 -0700

    :pencil: Module Docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,39 +1,29 @@
 # == Class: wkhtmltox
 #
-# Full description of class wkhtmltox here.
+# Installs wkhtmltopdf and wkhtmltoimage. 
 #
 # === Parameters
 #
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# [*version*]
+#   Specifies the version of wkhtmltox to install.
 #
 # === Examples
 #
+#  include wkhtmltox
+#
+# Or:
+#
 #  class { wkhtmltox:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#    version => '0.12.2.1',
 #  }
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Todd Bealmear <todd@t0dd.io>
 #
 # === Copyright
 #
-# Copyright 2015 Your name here, unless otherwise noted.
+# Copyright 2015 Todd Bealmear, unless otherwise noted.
 #
 class wkhtmltox ( $version = "0.12.2.1" ) {
   $base_url = "http://downloads.sourceforge.net/project/wkhtmltopdf"
